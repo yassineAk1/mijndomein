8cb7e8f276b0070cef3ea8c41a47bcde8d52f6c4
yassineAk1 <yassine9a6@gmail.com>
2026-06-22 15:52:01 +0200

    Add static assets

--- eleventy.config.js
+++ eleventy.config.js
@@ -3,6 +3,7 @@ import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
 export default function(eleventyConfig) {
   // 1. Copy your stylesheet
   eleventyConfig.addPassthroughCopy("styles.css");
+  eleventyConfig.addPassthroughCopy("assets");
 
   // 2. Configure the image plugin
   eleventyConfig.addPlugin(eleventyImageTransformPlugin);
